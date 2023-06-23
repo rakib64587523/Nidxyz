10025cca367906458bd07f33de4ace77c8bf2956
rakib64587523 <137508540+rakib64587523@users.noreply.github.com>
2023-06-23 18:19:33 +0600

    Add files via upload

---
+++ nid.nil-zone.xyz/index.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+
+
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>Login</title>
+     
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.png" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="assets/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Content -->
+
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Register -->
+          <div class="card">
+            <div class="card-body">
+              <!-- Logo -->
+			  
+			  
+              <div class="app-brand justify-content-center">
+			<!-----  <center> Registration Successfull <center> --->
+                <a href="verified-user-new-file-submission.php" class="app-brand-link gap-2">
+                  <span class="app-brand-logo demo">
+
+                  </span>
+                  <span class="app-brand-text  text-body fw-bolder"></span>
+                </a>
+              </div>
+              
+              <!-- /Logo -->
+             
+               <center><span class="app-brand-logo demo"><img width="100px" <img height="100px"src="assets/img/logo.png"></center>
+              <center><h3 style="color:red;class center="mb-2">Welcome</h3></center>
+              <center><p style="color:green; class="mb-4">ইমেইল এবং পাসওয়ার্ড দিয়ে লগ ইন করুন</p></center>
+              
+              
+		  <p style="color:red;" class="mb-4"></p>	  
+<form class="loginform" action="" method="POST">
+                <div class="mb-3">
+                  <label for="" class="text-uppercase text-sm">Your Email</label>
+									<input type="text" placeholder="Username" name="username" class="form-control mb" required>
+
+									<label for="" class="text-uppercase text-sm">Password</label>
+									<input type="password" placeholder="Password" name="password" class="form-control mb" required>
+									<button class="btn btn-primary d-grid w-100" name="login" type="submit">LOGIN</button>
+				
+			  </form>
+								<br>
+								<p>Don't Have an Account? <a href="register.php">Signup</a></p>
+
+								<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+								<a href="https://api.whatsapp.com/send/?phone=8801716926729&text&type=phone_number&app_absent=0" class="float" target="_blank">
+			  
+          
+			  
+            </div>
+          </div>
+          <!-- /Register -->
+        </div>
+      </div>
+    </div>
+
+    <!-- / Content -->
+
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="assets/vendor/libs/popper/popper.js"></script>
+    <script src="assets/vendor/js/bootstrap.js"></script>
+    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
